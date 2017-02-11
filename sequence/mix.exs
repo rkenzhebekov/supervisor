cbf40e3a9102505440c8a8612d3ce45954760c47
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2017-02-11 23:28:46 +0600

    Set server state from env var

--- sequence/mix.exs
+++ sequence/mix.exs
@@ -15,8 +15,12 @@ defmodule Sequence.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
-     mod: {Sequence, []}]
+    [
+      extra_applications: [:logger],
+      mod: {Sequence, []},
+      env: [initial_number: 456],
+      registered: [ Sequence.Server ]
+     ]
   end
 
   # Dependencies can be Hex packages:
